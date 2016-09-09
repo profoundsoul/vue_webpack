f81f9e69484c79c15038788a13481bc940e85937
linq <qiulin@uuch.net>
2016-09-09 12:24:48 +0800

    add chunk plugin

--- wpdemo/chunkplugin/webpack.config.js
+++ wpdemo/chunkplugin/webpack.config.js
@@ -3,18 +3,44 @@
  */
 
 var webpack = require("webpack");
+
+var allCommonChunk = new webpack.optimize.CommonsChunkPlugin({
+    name:'common',
+    //filename:'common.js'
+    //chunks:['a','b'],
+    //minChunks:2
+});
+var vendorChunk = new webpack.optimize.CommonsChunkPlugin({
+    name:'vendor1',
+    chunks:['vendor1', 'verdor2'],
+    minChunks:Infinity
+});
+
+var htmlPlugin = new require('html-webpack-plugin');
+
+
 module.exports={
     entry:{
         a:'./a',
-        b:'./b'
+        b:'./b',
+        //vendor:['../../libs/jquery-1.12.3', '../../libs/require', , '../../libs/underscore', '../../libs/backbone']
     },
     output:{
         path:'./dist',
         publicPath:'./dist/',
-        filename:'[name].[chunkhash:8].js',
+        filename:'[name].js',
         chunkFilename:'[name].chunk.js'
+        //filename:'[name].[chunkhash:8].js',
+        //chunkFilename:'[name].chunk.js'
+        // 把依赖打包到一个独立入口中
+        //library: ["MyLibrary", "[name]"],
+        //libraryTarget: "umd"
     },
     plugins:[
-        new webpack.optimize.CommonsChunkPlugin('commin.js')
-    ]
+        allCommonChunk
+    ],
+    externals:{
+        jquery: 'jQuery',
+        underscore:'_'
+    }
 };
